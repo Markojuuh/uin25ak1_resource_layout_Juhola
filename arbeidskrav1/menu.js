31a55bde4650f3075ced46d55af5af8873d18212
codeawayjustcodeaway <112389416+Markojuuh@users.noreply.github.com>
2025-01-31 11:25:40 +0100

    fixed the javascript and working on css to finish project

--- arbeidskrav1/menu.js
+++ arbeidskrav1/menu.js
@@ -2,7 +2,7 @@
 
 //resources.map(resource => resourceHTML += `)
 
-const menu = document.getElementById("menu")
+/*const menu = document.getElementById("menu")
 const main = document.querySelector("main")
 //hente kategorier fra ressurser
 const categories = resources.map(resource => resource.category)
@@ -54,4 +54,39 @@ displayMenu("HTML")
 //Satt HTML som active tab
 document.querySelector(`li[data-category="HTML"]`)?.classList.add("active")
 
-console.log("Menu laget:" , categories)
+console.log("Menu laget:" , categories)*/
+
+let resourceHTML = ""
+
+resources.map(resource => { resourceHTML += `
+   <button class="tab" onclick="showContent('${resource.category}')" >${resource.category}</button>
+   `
+})
+
+document.getElementById("menu").innerHTML = resourceHTML
+
+function showContent(categoryname) {
+    const filter = resources.filter(resource => resource.category === categoryname)
+    const selectedCategory = filter[0]
+    const contentHTML = `
+            <h2 id="header">${selectedCategory.category}</h2>
+            <p id = "text">${selectedCategory.text}</p>
+            <nav id= "srclinks">
+                <ul>
+                    ${selectedCategory.sources.map(source => `<li><a href="${source.url}" target="_blank">${source.title}</a></li>`)
+                .join('')}
+                </ul>
+            </nav>
+         `
+const contentContainer = document.getElementById("contentVisible")
+contentContainer.innerHTML = contentHTML
+
+contentContainer.classList.add("content-box")
+
+document.getElementById("contentVisible").innerHTML = contentHTML
+document.querySelectorAll(".tab").forEach(tab => tab.classList.remove("active"))
+document.querySelector(`button[onclick="showContent('${categoryname}')"]`).classList.add("active")
+
+}
+
+showContent("HTML")
